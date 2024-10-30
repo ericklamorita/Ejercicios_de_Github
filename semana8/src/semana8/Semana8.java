75f1dd6cb50a5d0771455d2321b6f957ae20a5fc
stevenarias65 <stevenarias65@gmail.com>
2024-10-29 19:37:06 -0600

    Update Semana8.java

--- semana8/src/semana8/Semana8.java
+++ semana8/src/semana8/Semana8.java
@@ -16,7 +16,7 @@ public class Semana8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("asd");
-        System.out.println("ajsndkandjkasnjkndkjandjan");
+        System.out.println("Hola mundo");
     }
     
 }
